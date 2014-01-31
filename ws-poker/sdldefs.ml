9325fefa3591db853772e30a7ad5b4dd867eb4cd
Piotr Mardziel <piotrm@kairos-alpha.local>
2014-01-31 15:59:37 -0500

    forgot

---
+++ ws-poker/sdldefs.ml
@@ -0,0 +1,7 @@
+type window = {mutable width: int;
+               mutable height: int;
+               mutable win: Sdlwindow.t;
+               mutable widthf: float;
+               mutable heightf: float;
+               mutable aspect: float};;
+
